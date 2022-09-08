0e52dba680dd937007fa021312e5a4296ff3560b
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2022-09-08 18:38:46 +1000

    project setup

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import { Html, Head, Main, NextScript } from "next/document";
+export function Documents() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@700&family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+         <Main />
+         <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ src/pages/products/[id].tsx
@@ -0,0 +1,7 @@
+import { useRouter } from "next/router";
+
+
+export function pages() {
+  //const {query} = useRouter;
+  return <div>Hello</div>;
+}
