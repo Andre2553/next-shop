7c12b81b5bc8d43ee7ea4534ae1ad8301ac8b856
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2022-11-09 13:51:40 +1100

    Header style

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { getCssText } from "../styles";
-export function Documents() {
+export default function Documents() {
   return (
     <Html>
       <Head>
---
+++ src/styles/pages/app.ts
@@ -0,0 +1,16 @@
+import { styled } from "..";
+
+export const Container = styled('div', {
+   display: 'flex',
+   flexDirection: 'column',
+   alignItems: "flex-start",
+   justifyContent: "center",
+   minWidth: '100vw',
+})
+
+export const Header = styled('header', {
+   padding: '2rem 0',
+   width: '100%',
+   maxWidth: '1100px',
+   margin: '0 auto',
+})
